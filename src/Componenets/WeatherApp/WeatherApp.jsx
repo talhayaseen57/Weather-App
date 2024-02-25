d32297edda898f0c3d46967aa427b7b7e03b0690
talhayaseen57 <talhayaseen5725@gmail.com>
2024-02-25 22:26:15 +0100

    Added search_func for search icon

--- src/Componenets/WeatherApp/WeatherApp.jsx
+++ src/Componenets/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 
 import search_icon from '../Assets/search.png'
@@ -11,19 +11,79 @@ import snow_icon from '../Assets/snow.png'
 import wind_icon from '../Assets/wind.png'
 
 export const WeatherApp = () => {
+
+    let api_key = "bb153afb243bf1617b48b328cf3931e5";
+
+    const [weatherIcon, setWeatherIcon] = useState(cloud_icon);
+
+    const search_func = async () => {
+        const city_element = document.getElementsByClassName("cityInput");
+        let city_name = city_element[0].value;
+
+        if (city_name === "") {
+            return 0;
+        }
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&&units=Metric&appid=${api_key}`;
+
+        let response = await fetch(url);
+        let json_data = await response.json();
+        
+        const current_temperature_element = document.getElementsByClassName("weather-temp");
+        const location_element = document.getElementsByClassName("weather-location");
+        const humidity_percentage_element = document.getElementsByClassName("humidity-percentage");
+        const wind_speed_element = document.getElementsByClassName("wind-speed");
+
+        current_temperature_element[0].innerHTML = json_data.main.temp + "°C";
+        location_element[0].innerHTML = json_data.name;
+        humidity_percentage_element[0].innerHTML = json_data.main.humidity + "%";
+        wind_speed_element[0].innerHTML = json_data.wind.speed + " km/h";
+
+        let weather_icon_code = json_data.weather[0].icon;
+        console.log(weather_icon_code);
+        console.log(typeof(weather_icon_code));
+
+        if (weather_icon_code === "01d" || weather_icon_code === "01n") {
+            setWeatherIcon(clear_icon);
+        }
+        else if (weather_icon_code === "02d" || weather_icon_code === "02n") {
+            setWeatherIcon(cloud_icon);
+        }
+        else if (weather_icon_code === "03d" || weather_icon_code === "03n") {
+            setWeatherIcon(drizzle_icon);
+        }
+        else if (weather_icon_code === "04d" || weather_icon_code === "04n") {
+            console.log("Function came here!");
+            setWeatherIcon(drizzle_icon);
+        }
+        else if (weather_icon_code === "09d" || weather_icon_code === "09n") {
+            setWeatherIcon(rain_icon);
+        }
+        else if (weather_icon_code === "10d" || weather_icon_code === "10n") {
+            setWeatherIcon(rain_icon);
+        }
+        else if (weather_icon_code === "13d" || weather_icon_code === "13n") {
+            setWeatherIcon(snow_icon);
+        }
+        else {
+            setWeatherIcon(clear_icon);
+        }
+
+    }
+
     return (
         <div className='container'>
             {/* Top Bar containing search input field and search icon */}
             <div className='top-bar'>
                 <input type="text" className='cityInput' placeholder='Search a city' />
-                <div className='search-icon'>
+                <div className='search-icon' onClick={() => {search_func()}}>
                     <img src={search_icon} alt="Search Icon" />
                 </div>
             </div>
 
             {/* div containing icon showing the current weather */}
             <div className="weather-icon">
-                <img src={cloud_icon} alt="Weather Icon" />
+                <img src={weatherIcon} alt="Weather Icon" />
             </div>
 
             {/* div containing temperature */}
