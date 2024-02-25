207e76a42cc1968e11cfd26c270ce49fb5843315
talhayaseen57 <talhayaseen5725@gmail.com>
2024-02-25 02:07:34 +0100

    Created and mounted WeatherApp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { WeatherApp } from './Componenets/WeatherApp/WeatherApp';
 
 function App() {
   return (
     <div className="App">
-      
+      <WeatherApp/>
     </div>
   );
 }
---
+++ src/Componenets/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './WeatherApp.css'
+
+export const WeatherApp = () => {
+  return (
+    <div>WeatherApp</div>
+  )
+}
